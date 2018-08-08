a1c54d10b1f2a3e23f6c305bfdbc41027b574d5a
Celine Gauchey <celinegauchey@gmail.com>
2018-08-08 17:24:45 -0400

    basic structure set up

--- App.js
+++ App.js
@@ -1,18 +1,51 @@
 import React from "react";
 import { createStackNavigator } from "react-navigation";
-import { Home } from "./components";
+import {
+  Home,
+  Convos,
+  LogIn,
+  Settings,
+  SignUp,
+  SingleConvo
+} from "./client/components";
 
-const RootStack = createStackNavigator({
+const RootNavigator = createStackNavigator({
   Home: {
     screen: Home,
     navigationOptions: {
       headerTitle: "Home"
     }
+  },
+  Convos: {
+    screen: Convos,
+    navigationOptions: {
+      headerTitle: "Conversations"
+    }
+  },
+  LogIn: {
+    screen: LogIn,
+    navigationOptions: {
+      headerTitle: "Log In"
+    }
+  },
+  Settings: {
+    screen: Settings,
+    navigationOptions: {
+      headerTitle: "Settings"
+    }
+  },
+  SignUp: {
+    screen: SignUp,
+    navigationOptions: {
+      headerTitle: "SignUp"
+    }
+  },
+  SingleConvo: {
+    screen: SingleConvo,
+    navigationOptions: {
+      headerTitle: "Single Conversation"
+    }
   }
 });
 
-export default class App extends React.Component {
-  render() {
-    return <RootStack />;
-  }
-}
+export default RootNavigator;
---
+++ client/components/Convos.js
@@ -0,0 +1,12 @@
+// this can be deleted
+
+import React from "react";
+import { Text, View } from "react-native";
+
+const Convos = ({ navigation }) => (
+  <View>
+    <Text>Conversations go here</Text>
+  </View>
+);
+
+export default Convos;
