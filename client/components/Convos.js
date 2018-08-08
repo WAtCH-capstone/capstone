5992e13a58a75e0700f1d64f4916387b69255a28
Andrew Miller <admiller9@gmail.com>
2018-08-08 19:34:18 -0400

    removed console logs

--- client/components/Convos.js
+++ client/components/Convos.js
@@ -1,15 +1,11 @@
-// this can be deleted
-
 import React from 'react';
 import { Text, View, Button } from 'react-native';
 import styles from './Styles';
 
 const Convos = ({ navigation }) => {
-  console.log(navigation.state.params.convos);
   return (
     <View>
       {navigation.state.params.convos.map(convo => {
-        console.log(navigation.state.params.convos.messages);
         const firstMessage = convo.messages[0];
         return (
           <View key={convo.id}>
