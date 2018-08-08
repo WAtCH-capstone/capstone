bfdd3812369cfacc5f91bba6941b81cd9048516c
Hadley Callaway <hccallaway@gmail.com>
2018-08-08 17:39:09 -0400

    deleted unuse react import from app.js

--- App.js
+++ App.js
@@ -1,51 +1,50 @@
-import React from "react";
-import { createStackNavigator } from "react-navigation";
+import { createStackNavigator } from 'react-navigation';
 import {
   Home,
   Convos,
   LogIn,
   Settings,
   SignUp,
-  SingleConvo
-} from "./client/components";
+  SingleConvo,
+} from './client/components';
 
 const RootNavigator = createStackNavigator({
   Home: {
     screen: Home,
     navigationOptions: {
-      headerTitle: "Home"
-    }
+      headerTitle: 'Home',
+    },
   },
   Convos: {
     screen: Convos,
     navigationOptions: {
-      headerTitle: "Conversations"
-    }
+      headerTitle: 'Conversations',
+    },
   },
   LogIn: {
     screen: LogIn,
     navigationOptions: {
-      headerTitle: "Log In"
-    }
+      headerTitle: 'Log In',
+    },
   },
   Settings: {
     screen: Settings,
     navigationOptions: {
-      headerTitle: "Settings"
-    }
+      headerTitle: 'Settings',
+    },
   },
   SignUp: {
     screen: SignUp,
     navigationOptions: {
-      headerTitle: "SignUp"
-    }
+      headerTitle: 'SignUp',
+    },
   },
   SingleConvo: {
     screen: SingleConvo,
     navigationOptions: {
-      headerTitle: "Single Conversation"
-    }
-  }
+      headerTitle: 'Single Conversation',
+    },
+  },
 });
 
 export default RootNavigator;
