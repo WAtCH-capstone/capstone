4c89df1af772ace8390df5fc280ab581618ac5ae
Andrew Miller <admiller9@gmail.com>
2018-08-09 12:27:48 -0400

    Convos component is styled, each, row links to single convo (removed button underneath)

--- client/components/Convos.js
+++ client/components/Convos.js
@@ -1,29 +1,57 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
-import styles from './Styles';
+import { Button } from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Left,
+  Body,
+  Right,
+  Thumbnail,
+  Text,
+  Title,
+} from 'native-base';
 
 const Convos = ({ navigation }) => {
   return (
-    <View>
-      {navigation.state.params.convos.map(convo => {
-        const firstMessage = convo.messages[0];
-        return (
-          <View key={convo.id}>
-            <View>
-              <Text>
-                {convo.name} {firstMessage.text} {firstMessage.time}
-              </Text>
-            </View>
-            <View>
-              <Button
+    <Container>
+      <Header>
+        <Title>My Conversations</Title>
+      </Header>
+      <Content>
+        <List>
+          {navigation.state.params.convos.map(convo => {
+            const firstMessage = convo.messages[0];
+            return (
+              <ListItem
+                avatar
+                key={convo.id}
                 onPress={() => navigation.navigate('SingleConvo', { convo })}
-                title="View Conversation"
-              />
-            </View>
-          </View>
-        );
-      })}
-    </View>
+              >
+                <Left>
+                  <Thumbnail
+                    source={{ uri: 'https://placeimg.com/140/140/any' }}
+                  />
+                </Left>
+                <Body>
+                  <Text>{convo.name}</Text>
+                  <Text note>{firstMessage.text}.</Text>
+                </Body>
+                <Right>
+                  <Text note>{firstMessage.time}</Text>
+                </Right>
+                {/* <Button
+                  onPress={() => navigation.navigate('SingleConvo', { convo })}
+                  title="View Conversation"
+                /> */}
+              </ListItem>
+            );
+          })}
+        </List>
+      </Content>
+    </Container>
   );
 };
 
