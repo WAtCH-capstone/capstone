d726ecd598a6855ba53ab3f50ebbcb0b74a9524b
Hadley Callaway <hccallaway@gmail.com>
2018-08-09 11:03:04 -0400

    testing console logs

--- client/components/Settings.js
+++ client/components/Settings.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
 
 export default class Settings extends Component {
+  test() {
+    console.log('hi');
+  }
+
   render() {
     const navigation = this.props.navigation;
     return (
       <View>
         <Text>Settings goes here</Text>
+        <Button onPress={() => this.test()} title="test" />
       </View>
     );
   }
