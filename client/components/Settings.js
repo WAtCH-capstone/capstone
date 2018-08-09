bf9b433ed2b15e89695cfada27d52aeb8b12aa6a
Hadley Callaway <hccallaway@gmail.com>
2018-08-09 11:51:13 -0400

    working on settings component

--- client/components/Settings.js
+++ client/components/Settings.js
@@ -1,18 +1,70 @@
 import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  ListItem,
+  Text,
+  Separator,
+  Card,
+  CardItem,
+  Thumbnail,
+  Left,
+  Body,
+  Button,
+  List,
+} from 'native-base';
 
 export default class Settings extends Component {
-  test() {
-    console.log('hi');
-  }
-
   render() {
     const navigation = this.props.navigation;
     return (
-      <View>
-        <Text>Settings goes here</Text>
-        <Button onPress={() => this.test()} title="test" />
-      </View>
+      <Container>
+        <Header />
+        <Content>
+          <Separator bordered>
+            <Text>Your Profile</Text>
+          </Separator>
+          <Card>
+            <CardItem>
+              <Left>
+                <Thumbnail
+                  source={{
+                    uri:
+                      'https://lh3.googleusercontent.com/vgv0EDmcYrsy-o7ZjRzKPbJzW2fC7uqSKsnMhrGcTaMImLIKM-1ePl0Gy-n-8SFmCYJKWUf-wu4ChBkJAQ',
+                  }}
+                />
+                <Body>
+                  <Text>Filler Name</Text>
+                  <Text note>username123</Text>
+                  <Text note>email@gmail.com</Text>
+                </Body>
+              </Left>
+            </CardItem>
+          </Card>
+
+          {/* <ListItem>
+            <Text>Username</Text>
+          </ListItem>
+          <ListItem last>
+            <Text>Email</Text>
+          </ListItem> */}
+          <Separator bordered>
+            <Text>Options</Text>
+          </Separator>
+          <List>
+            <ListItem selected>
+              <Text>Edit your profile</Text>
+            </ListItem>
+            <ListItem selected>
+              <Text>Change your password</Text>
+            </ListItem>
+            <ListItem selected last>
+              <Text>Logout</Text>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
     );
   }
 }
