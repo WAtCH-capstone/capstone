8c6677ac768e40110647247c32ae672dfa1b43e6
Andrew Miller <admiller9@gmail.com>
2018-08-09 15:49:18 -0400

    conversations component renders data from our database

--- client/components/Convos.js
+++ client/components/Convos.js
@@ -15,47 +15,54 @@ import db from '../../firestore';
 export default class Convos extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { convos: null };
   }
 
   componentDidMount() {
-    const convo = db.collection('conversations').doc('ZtaxF3W255TD6CdjB144');
-    // const convo = db.ref('conversations/ZtaxF3W255TD6CdjB144');
-    console.log(convo);
+    db.collection('conversations')
+      .doc('ZtaxF3W255TD6CdjB144')
+      .get()
+      .then(snapshot => this.setState({ convos: snapshot.data() }));
   }
 
   render() {
+    console.log(this.state.convos);
     const navigation = this.props.navigation;
-    return (
-      <Container>
-        <Content>
-          <List>
-            {navigation.state.params.convos.map(convo => {
-              const firstMessage = convo.messages[0];
-              return (
-                <ListItem
-                  avatar
-                  key={convo.id}
-                  onPress={() => navigation.navigate('SingleConvo', { convo })}
-                >
-                  <Left>
-                    <Thumbnail
-                      source={{ uri: 'https://placeimg.com/140/140/any' }}
-                    />
-                  </Left>
-                  <Body>
-                    <Text>{convo.name}</Text>
-                    <Text note>{firstMessage.text}.</Text>
-                  </Body>
-                  <Right>
-                    <Text note>{firstMessage.time}</Text>
-                  </Right>
-                </ListItem>
-              );
-            })}
-          </List>
-        </Content>
-      </Container>
-    );
+    if (this.state.convos) {
+      const convos = this.state.convos;
+      const firstMessage = convos.messages[0];
+      return (
+        <Container>
+          <Content>
+            <List>
+              <ListItem
+                key={1}
+                avatar
+                onPress={() => navigation.navigate('SingleConvo', { convos })}
+              >
+                <Left>
+                  <Thumbnail
+                    source={{ uri: 'https://placeimg.com/140/140/any' }}
+                  />
+                </Left>
+                <Body>
+                  <Text>{convos.users[0].name}</Text>
+                  <Text note>{firstMessage.text}.</Text>
+                </Body>
+                <Right>
+                  <Text note>{firstMessage.time}</Text>
+                </Right>
+              </ListItem>
+            </List>
+          </Content>
+        </Container>
+      );
+    } else {
+      return (
+        <Container>
+          <Text>Getting data...</Text>
+        </Container>
+      );
+    }
   }
 }
