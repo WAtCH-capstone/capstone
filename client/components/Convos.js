3f026d8d264099ffa8783631d9bfb125c44907ff
Andrew Miller <admiller9@gmail.com>
2018-08-09 15:27:18 -0400

    wip

--- client/components/Convos.js
+++ client/components/Convos.js
@@ -10,39 +10,52 @@ import {
   Thumbnail,
   Text,
 } from 'native-base';
+import db from '../../firestore';
 
-const Convos = ({ navigation }) => {
-  return (
-    <Container>
-      <Content>
-        <List>
-          {navigation.state.params.convos.map(convo => {
-            const firstMessage = convo.messages[0];
-            return (
-              <ListItem
-                avatar
-                key={convo.id}
-                onPress={() => navigation.navigate('SingleConvo', { convo })}
-              >
-                <Left>
-                  <Thumbnail
-                    source={{ uri: 'https://placeimg.com/140/140/any' }}
-                  />
-                </Left>
-                <Body>
-                  <Text>{convo.name}</Text>
-                  <Text note>{firstMessage.text}.</Text>
-                </Body>
-                <Right>
-                  <Text note>{firstMessage.time}</Text>
-                </Right>
-              </ListItem>
-            );
-          })}
-        </List>
-      </Content>
-    </Container>
-  );
-};
+export default class Convos extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-export default Convos;
+  componentDidMount() {
+    const convo = db.collection('conversations').doc('ZtaxF3W255TD6CdjB144');
+    // const convo = db.ref('conversations/ZtaxF3W255TD6CdjB144');
+    console.log(convo);
+  }
+
+  render() {
+    const navigation = this.props.navigation;
+    return (
+      <Container>
+        <Content>
+          <List>
+            {navigation.state.params.convos.map(convo => {
+              const firstMessage = convo.messages[0];
+              return (
+                <ListItem
+                  avatar
+                  key={convo.id}
+                  onPress={() => navigation.navigate('SingleConvo', { convo })}
+                >
+                  <Left>
+                    <Thumbnail
+                      source={{ uri: 'https://placeimg.com/140/140/any' }}
+                    />
+                  </Left>
+                  <Body>
+                    <Text>{convo.name}</Text>
+                    <Text note>{firstMessage.text}.</Text>
+                  </Body>
+                  <Right>
+                    <Text note>{firstMessage.time}</Text>
+                  </Right>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Content>
+      </Container>
+    );
+  }
+}
