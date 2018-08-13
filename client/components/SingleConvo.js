fd730c0619ca944db1ff9b59dc43456a1c13edfa
Celine Gauchey <celinegauchey@gmail.com>
2018-08-13 18:15:34 -0400

    fixing header

--- client/components/SingleConvo.js
+++ client/components/SingleConvo.js
@@ -1,50 +1,50 @@
-import React from 'react';
+import React from "react";
 import {
   Text,
   View,
   KeyboardAvoidingView,
   StyleSheet,
   Image,
-  Button,
-} from 'react-native';
-import Messages from './Messages';
-import SingleConvoPreferences from './SingleConvoPreferences';
-import SideMenu from 'react-native-side-menu';
-import db from '../../firestore';
+  Button
+} from "react-native";
+import Messages from "./Messages";
+import SingleConvoPreferences from "./SingleConvoPreferences";
+import SideMenu from "react-native-side-menu";
+import db from "../../firestore";
 
 export default class SingleConvo extends React.Component {
   constructor() {
     super();
     this.state = {
-      id: '',
+      id: "",
       messages: [],
       friend: {},
-      menuOpen: false,
+      menuOpen: false
     };
   }
 
   async componentDidMount() {
-    console.log('mounting single convo');
+    console.log("mounting single convo");
     const navProps = this.props.navigation.state.params;
     const friend = navProps.friend;
     const id = navProps.id;
     let messages = await db
-      .collection('conversations')
+      .collection("conversations")
       .doc(id)
-      .collection('messages')
-      .orderBy('createdAt', 'desc')
+      .collection("messages")
+      .orderBy("createdAt", "desc")
       .get();
     messages = messages.docs.map(el => el.data());
     this.setState({
       id,
       messages,
-      friend,
+      friend
     });
   }
   render() {
     const userImage = {
       uri:
-        'https://lh3.googleusercontent.com/vgv0EDmcYrsy-o7ZjRzKPbJzW2fC7uqSKsnMhrGcTaMImLIKM-1ePl0Gy-n-8SFmCYJKWUf-wu4ChBkJAQ',
+        "https://lh3.googleusercontent.com/vgv0EDmcYrsy-o7ZjRzKPbJzW2fC7uqSKsnMhrGcTaMImLIKM-1ePl0Gy-n-8SFmCYJKWUf-wu4ChBkJAQ"
     };
     const menu = <SingleConvoPreferences navigator={navigator} />;
     if (this.state.messages && this.state.messages.length) {
@@ -52,14 +52,14 @@ export default class SingleConvo extends React.Component {
         <SideMenu menu={menu} menuPosition="right" isOpen={this.state.menuOpen}>
           <View style={styles.container}>
             {/* add padding, change to keyboard avoiding view*/}
-            <View style={{ flex: 3, flexDirection: 'row' }}>
+            <View style={{ flex: 3, flexDirection: "row" }}>
               <View style={{ width: 60, height: 60 }}>
                 <Image source={userImage} style={styles.image} />
               </View>
-              <View style={{ width: 170, height: 170 }}>
+              <View style={{ width: 170, height: 60 }}>
                 <Text>{this.state.friend.displayName}</Text>
               </View>
-              <View style={{ width: 150, height: 150 }}>
+              <View style={{ width: 150, height: 60 }}>
                 <Button
                   title="Preferences"
                   onPress={() => {
@@ -82,12 +82,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingBottom: 50,
-    backgroundColor: 'white',
+    backgroundColor: "white"
   },
   image: {
     width: 50,
-    height: 50,
-  },
+    height: 50
+  }
 });
 
 // console.disableYellowBox = true;
