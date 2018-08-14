21c335fb8f2dc476a3f026615acfe4f6a4fe9f89
Andrew Miller <admiller9@gmail.com>
2018-08-14 17:29:01 -0400

    merged messages and singleconvo components, edited cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,19 @@
 const functions = require('firebase-functions');
+const schedule = require('node-schedule');
+const db = require('../firestore');
 
-exports.onSend = functions.firestore
-  .document('conversations/{convoID}/messages/{messageID}')
+exports.trigger = functions.firestore
+  .document(`triggers/{triggerID}`)
   .onCreate((snap, context) => {
-    console.log(snap.data(), context);
+    console.log('context var', context);
+    const trigger = snap.data();
+    if (trigger.time) {
+      schedule.scheduleJob('second minute hour day month', () =>
+        db
+          .collection('conversations')
+          .doc(conversationID)
+          .collection('messages')
+          .add(newMessage)
+      );
+    }
   });
