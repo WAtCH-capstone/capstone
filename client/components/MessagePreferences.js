dff704113fa70303d871df2b1017958152dd530a
Wayne Hong <woo.young.hong@hotmail.com>
2018-08-20 11:31:52 -0400

    location works well

--- client/components/MessagePreferences.js
+++ client/components/MessagePreferences.js
@@ -36,25 +36,71 @@ export default class MessagePreferences extends Component {
       currentLat: null,
       currentLong: null,
       error: null,
-      distanceLeft: '',
       showMsg: false,
+      locationTrigger: false,
     };
     this.setTrigger = this.setTrigger.bind(this);
     this._handleDatePicked = this._handleDatePicked.bind(this);
     this._handlePress = this._handlePress.bind(this);
     this.onSend = this.onSend.bind(this);
     this.user = firebase.auth().currentUser;
+    this.getDistanceFromDestination = this.getDistanceFromDestination.bind(
+      this
+    );
   }
 
   componentDidMount() {
     const ref = this.getRef(this.props.navigation.state.params.id);
     this.setState({ ref });
+    // setInterval(() => {
+    //   this.watchId = navigator.geolocation.watchPosition(
+    //     position => {
+    //       // const initialPosition = JSON.stringify(position);
+    //       this.setState({
+    //         currentLat: position.coords.latitude,
+    //         currentLong: position.coords.longitude,
+    //       });
+    //     },
+    //     error => this.setState({ error: error.message }),
+    //     {
+    //       enableHighAccuracy: true,
+    //       timeout: 20000,
+    //       maximumAge: 1000,
+    //       distanceFilter: 10,
+    //     }
+    //   );
+    //   if (this.state.currentLat && this.state.currentLong) {
+    //     this.getDistanceFromDestination();
+    //     if (this.state.distanceFromAtoB < 502) {
+    //       this.onSend();
+    //     } else {
+    //       console.log('msg will not get sent');
+    //     }
+    //   }
+    // }, 10000);
   }
+  // componentWillUnmount() {
+  //   navigator.geolocation.clearWatch(this.watchId);
+  // }
 
-  getDistanceFromDestination() {}
-
-  componentWillUnmount() {
-    navigator.geolocation.clearWatch(this.watchId);
+  getDistanceFromDestination() {
+    if (this.state.locationDetails.hasOwnProperty('geometry')) {
+      const dist = geodist(
+        { lat: this.state.currentLat, long: this.state.currentLong },
+        {
+          lat: this.state.locationDetails.geometry.location.lat,
+          long: this.state.locationDetails.geometry.location.lng,
+        },
+        { unit: 'feet' }
+      );
+      // console.log('first coord', this.state.locationDetails.geometry.location.lat);
+      // this.setState({ distanceFromAtoB: dist });
+      console.log(typeof dist);
+      console.log('distance from current to destination: ', dist);
+      this.setState({
+        distanceFromAtoB: dist,
+      });
+    }
   }
 
   getRef(id) {
@@ -81,73 +127,105 @@ export default class MessagePreferences extends Component {
       locationDetails: res,
       textInput: el.description,
       showResults: false,
+      locationTrigger: !this.state.locationTrigger,
     });
   };
 
   async onSend() {
-    let createdAt;
-    const date = new Date(this.state.triggers.date);
-    createdAt = date.getTime();
-    const newMessage = {
-      _id: createdAt,
-      text: this.props.navigation.state.params.messageContent,
-      createdAt,
-      user: { _id: this.props.navigation.state.params.user.uid },
-    };
-    db.collection('users')
-      .doc(this.user.uid)
-      .collection('scheduled')
-      .add({ newMessage, convoID: this.props.navigation.state.params.id });
-    schedule.scheduleJob(date, () => {
-      this.state.ref.collection('messages').add(newMessage);
-      this.state.ref.set({ firstMessage: newMessage }, { merge: true });
-    });
+    // checking current coordinates and send based on whether user is within 0.1 miles
+    if (this.state.locationTrigger) {
+      let createdAt;
+      const date = new Date();
+      createdAt = date.getTime();
+      const newMessage = {
+        _id: createdAt,
+        text: this.props.navigation.state.params.messageContent,
+        createdAt,
+        user: { _id: this.props.navigation.state.params.user.uid },
+      };
+      db.collection('users')
+        .doc(this.user.uid)
+        .collection('scheduled')
+        .add({ newMessage, convoID: this.props.navigation.state.params.id });
+
+      this.interval = setInterval(() => {
+        if (this.state.locationDetails) {
+          console.log('i am here');
+          this.watchId = navigator.geolocation.watchPosition(
+            position => {
+              this.setState({
+                currentLat: position.coords.latitude,
+                currentLong: position.coords.longitude,
+              });
+            },
+            error => this.setState({ error: error.message }),
+            {
+              enableHighAccuracy: true,
+              timeout: 20000,
+              maximumAge: 1000,
+              distanceFilter: 10,
+            }
+          );
+          console.log(
+            'current coords,',
+            this.state.currentLong,
+            this.state.currentLat
+          );
+          if (this.state.currentLat && this.state.currentLong) {
+            this.getDistanceFromDestination();
+            console.log(this.state.distanceFromAtoB);
+            if (this.state.distanceFromAtoB < 502) {
+              console.log('message will get sent');
+              this.state.ref.collection('messages').add(newMessage);
+              this.state.ref.set({ firstMessage: newMessage }, { merge: true });
+              clearInterval(this.interval);
+            } else {
+              console.log('msg will not get sent');
+            }
+          }
+        }
+      }, 1000);
+    } else {
+      let createdAt;
+      const date = new Date(this.state.triggers.date);
+      createdAt = date.getTime();
+      const newMessage = {
+        _id: createdAt,
+        text: this.props.navigation.state.params.messageContent,
+        createdAt,
+        user: { _id: this.props.navigation.state.params.user.uid },
+      };
+      db.collection('users')
+        .doc(this.user.uid)
+        .collection('scheduled')
+        .add({ newMessage, convoID: this.props.navigation.state.params.id });
+      schedule.scheduleJob(date, () => {
+        this.state.ref.collection('messages').add(newMessage);
+        this.state.ref.set({ firstMessage: newMessage }, { merge: true });
+      });
+    }
   }
 
   render() {
     const { isDateTimePickerVisible, triggers } = this.state;
-
-    this.watchId = navigator.geolocation.watchPosition(
-      position => {
-        // const initialPosition = JSON.stringify(position);
-        this.setState(
-          {
-            currentLat: position.coords.latitude,
-            currentLong: position.coords.longitude,
-          },
-          () => {
-            console.log('this.state', this.state);
-          }
-        );
-      },
-      error => this.setState({ error: error.message }),
-      {
-        enableHighAccuracy: true,
-        timeout: 20000,
-        maximumAge: 1000,
-        distanceFilter: 10,
-      }
-    );
-
-    if (this.state.locationDetails.hasOwnProperty('geometry')) {
-      const dist = geodist(
-        { lat: this.state.currentLat, long: this.state.currentLong },
-        {
-          lat: this.state.locationDetails.geometry.location.lat,
-          long: this.state.locationDetails.geometry.location.lng,
-        },
-        { unit: 'feet' }
-      );
-      // console.log('first coord', this.state.locationDetails.geometry.location.lat);
-      // this.setState({ distanceFromAtoB: dist });
-      console.log(typeof dist);
-      console.log('distance from current to destination: ', dist);
-      if (dist <= 528) {
-        // less than a foot
-        console.log('true!');
-      }
-    }
-
+    // testing:
+    // const newLat = 40.766291;
+    // const newLng = -73.923729;
+    // setTimeout(() => {
+    //   this.setState(
+    //     {
+    //       locationDetails: {
+    //         geometry: { location: { lat: newLat + 1, lng: newLng + 1 } },
+    //       },
+    //     },
+    //     () => {
+    //       console.log(
+    //         'time out after 5 seconds',
+    //         this.state.locationDetails.geometry.location
+    //       );
+    //     }
+    //   );
+    // }, 5000);
     return (
       <View>
         <View style={{ backgroundColor: 'white', paddingBottom: 540 }}>
